bdaae710df52f77b4a39798b1bf0a741138b9f42
Christian Quispe <wirkentod@gmail.com>
2017-02-09 15:28:52 -0500

    change1

--- AppEscalable/sort_directory.py
+++ AppEscalable/sort_directory.py
@@ -1,5 +1,5 @@
 import os
-dirname_traces='./AppEscalable/Traces'
+dirname_traces='../AppEscalable/Traces'
 for file_trace in sorted(os.listdir(dirname_traces)):
 	print "File primer niver: %s" %(file_trace)
 	for file in sorted(os.listdir(str(dirname_traces+'//'+file_trace))):
